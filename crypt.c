bd05c4a92c9bdb844a24007b8adc2bcedb1e97a1
Daniel Rouco Rodríguez <danielrouco188rr@gmail.com>
2024-05-05 21:23:48 +0200

    Adding decrypt function

--- crypt.c
+++ crypt.c
@@ -46,11 +46,31 @@ char* encrypt(char* code, int displacement){
     return encrypted;
 }
 
-int main() {
-    char* code = "daniel8"; 
-    char* encrypted = encrypt(code, 10);
+char* decrypt(char* code, int displacement){
+    char* displaced = displace(displacement);
     
-    printf("%s", encrypted);
+    int decrypted_length = length(code);
+    char* decrypted = malloc(decrypted_length * sizeof(char));
+    
+    for(int i = 0; i < decrypted_length - 1; i++){
+        int displaced_index = 0;
+        while(displaced[displaced_index] != code[i]){
+            displaced_index++;
+        }
+        decrypted[i] = symbols[displaced_index];
+    }
+    
+    return decrypted;
+}
+
+int main() {
+    char* code = "daniel15"; 
+    
+    char* encrypted = encrypt(code, 10);
+    printf("%s\n", encrypted);
+    
+    char* decrypted = decrypt(encrypted, 10);
+    printf("%s", decrypted);
     
     return 0;
 }
